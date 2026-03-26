c0f3da54efc4543bd9dff795fa6c049a14172883
Robin Ryan <robin1ryan@hotmail.com>
2026-03-26 08:56:32 +0200

    Feat(Views/Index): List now displays messages

--- Routes/index.js
+++ Routes/index.js
@@ -2,6 +2,19 @@ import { Router } from "express";
 
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.send("This is the root index"));
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+indexRouter.get("/", (req, res) => res.render("index", { messages }));
 
 export default indexRouter;
--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 import express from "express";
 import indexRouter from "./Routes/index.js";
 import newRouter from "./Routes/new.js";
+import path, { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
 // Set up a views dirname & engine
 // Set up a dirname for the css file
 
+// Define __filename because I am using modules, instead of commonjs
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const PORT = 3000;
 const app = express();
 
+// define a path for app to route to views
+app.set("views", path.join(__dirname, "views"));
+// Define EJS engine
+app.set("view engine", "ejs");
+
+// Roots
 app.use("/", indexRouter);
 app.use("/new", newRouter);
 
+// Listener
 app.listen(PORT, (err) => {
   if (err) {
     throw err;
