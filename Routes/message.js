d63cb70af3b2f3d49155791138e77ecc5b1d7e77
Robin Ryan <robin1ryan@hotmail.com>
2026-03-26 10:07:40 +0200

    /message now displays the entire message

--- Routes/message.js
+++ Routes/message.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import { messages } from "./index.js";
 
 const messageRoute = Router();
 
 messageRoute.get("/:id", (req, res) =>
-  res.render("message", { id: req.params.id }),
+  res.render("message", {
+    id: req.params.id,
+    message: messages[req.params.id],
+  }),
 );
 
 export default messageRoute;
