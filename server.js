87c76972eaaac42b20222d5b2d1af4f0726dea91
Robin Ryan <robin1ryan@hotmail.com>
2026-03-26 13:23:18 +0200

    chore: clean commit

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ import indexRouter from "./Routes/index.js";
 import newRouter from "./Routes/new.js";
 import messageRoute from "./Routes/message.js";
 
+import { neon } from "@neondatabase/serverless";
+import "dotenv/config";
+
+// Database
+const sql = neon(process.env.DATABASE_URL);
+
 // Set up a dirname for the css file
 
 // Define __filename because I am using modules, instead of commonjs
@@ -23,15 +29,24 @@ app.set("view engine", "ejs");
 // decode forms inputs
 app.use(express.urlencoded({ extended: true }));
 
+const requestHandler = async (req, res) => {
+  const result = await sql`SELECT version()`;
+  const { version } = result[0];
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(version);
+};
+
+// neon-test server
+app.get("/db-test", requestHandler);
+
 // Roots
 app.use("/", indexRouter);
 app.use("/new", newRouter);
 app.use("/message/", messageRoute);
 
-// Listener
 app.listen(PORT, (err) => {
   if (err) {
     throw err;
   }
-  console.log("App running");
+  console.log("App running at port: ", PORT);
 });
